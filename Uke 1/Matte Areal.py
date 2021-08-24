1d3062ed08186b931cc62ed3c5debeb670cc6af4
Herman <hfausk@outlook.com>
2021-08-24 13:16:54 +0200

    Lagt til Areal av firkant

---
+++ Uke 1/Matte Areal.py
@@ -0,0 +1,10 @@
+
+#Regne ut areal av en firkant
+Høyde = int (input("skriv Høyde"))
+Bredde = int (input("skriv bredde her"))
+
+Areal = Høyde*Bredde
+Omkrets = Høyde+Høyde+Bredde+Bredde
+
+print("Arealet til objectet er: " + str(Areal))
+print("omkretsen til objectet er: " + str(Omkrets))
